04ae5ad0eae8de2f02c4dc7da4d7b91af7933c0f
Jiajiabulusi <143712354+Jiajiabulusi@users.noreply.github.com>
2023-08-31 21:05:40 +0800

    Add files via upload
    Str for C

---
+++ str.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	float num2 = 0;
+//
+//	scanf_s("%d %f", &num1, &num2);
+//
+//	float sum = num1 + num2;
+//	
+//	printf("Sum = %f\n", sum);
+//
+//
+//	return 0;
+//}
+int main()
+{
+
+	/*char arr1[] = "acbs";
+	char arr2[] = {'a','c','b','s'};*/
+	printf("\a");
+	return 0;
+}
